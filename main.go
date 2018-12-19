2e9cd6cfe34b33952bf07bf95396edda60f87bad
ZenGround0 <ZenGround0@users.noreply.github.com>
2018-12-19 11:18:56 -0500

    change hi to allow for caps

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package testmod
 import "fmt"
 
 // Hi returns an unfriendly greeting
-func Hi(name string) string {
+func Hi(name string, caps bool) string {
+	if caps {
+		return fmt.Sprintf("PLEASE STOP THE HATE, %s", name)
+	}
 	return fmt.Sprintf("Please stop the hate, %s", name)
 }

a097b41982b3413077430dda41f4697f7e04d98c
ZenGround0 <ZenGround0@users.noreply.github.com>
2018-12-19 10:50:06 -0500

    Premier commitment

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/zenground0/testmod
---
+++ main.go
@@ -0,0 +1,8 @@
+package testmod
+
+import "fmt"
+
+// Hi returns an unfriendly greeting
+func Hi(name string) string {
+	return fmt.Sprintf("Get fucked, %s", name)
+}
